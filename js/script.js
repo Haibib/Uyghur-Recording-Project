3f9ddcbc1e19c9ddff64223c201932a9e8173d1a
Haibib Kerim <haibib.bibi@gmail.com>
2022-12-05 22:32:11 -0500

    tile update

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+document.getElementById("cards").onmousemove = e => {
+  for(const card of document.getElementsByClassName("card")) {
+    const rect = card.getBoundingClientRect(),
+          x = e.clientX - rect.left,
+          y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
 var selectors = document.getElementsByClassName("form-check-input");
 var hyphen = document.getElementsByClassName("hyphen-container");
 var gloss = document.getElementsByClassName("gloss-container");
