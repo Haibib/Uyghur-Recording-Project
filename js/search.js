386f4f2a5626e10b2921349c2984348ec13acc36
Haibib2 <66964771+Haibib2@users.noreply.github.com>
2024-06-12 18:04:12 -0400

    Update search.js

--- js/search.js
+++ js/search.js
@@ -101,37 +101,40 @@ function searchText() {
 function navigateToContext(url, contextId) {
     try {
         const currentUrl = window.location.href.split('#')[0];
-        if (currentUrl === url) {
-            // Scroll to the context if we are already on the same page
-            const element = document.getElementById(contextId);
-            if (element) {
-                const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        const element = document.getElementById(contextId);
+        if (element) {
+            const elementPosition = element.offsetTop;
+            sessionStorage.setItem('elementPosition', elementPosition);
+
+            if (currentUrl === url) {
+                // Scroll to the context if we are already on the same page
                 window.scrollTo({ top: elementPosition, behavior: 'smooth' });
                 element.classList.add('highlight');
+            } else {
+                // Navigate to the new page
+                sessionStorage.setItem('contextId', contextId);
+                window.location.href = url;
             }
-        } else {
-            // Use session storage to pass the context ID to the new page
-            sessionStorage.setItem('contextId', contextId);
-            window.location.href = url;
         }
     } catch (error) {
         console.error('Error navigating to context:', error);
     }
 }
-window.onload = function(){
-    fetchContentIndex();
-};
+
 // Check if there is a contextId in session storage and scroll to it
-window.onloadeddata = function() {
+window.onload = function() {
+    fetchContentIndex();
     const contextId = sessionStorage.getItem('contextId');
-    if (contextId) {
+    const elementPosition = sessionStorage.getItem('elementPosition');
+
+    if (contextId && elementPosition) {
+        window.scrollTo({ top: parseInt(elementPosition), behavior: 'smooth' });
         const element = document.getElementById(contextId);
         if (element) {
-            const elementPosition = element.getBoundingClientRect().top + window.scrollY;
-            window.scrollTo({ top: elementPosition, behavior: 'smooth' });
             element.classList.add('highlight');
         }
         sessionStorage.removeItem('contextId');
+        sessionStorage.removeItem('elementPosition');
     }
 };
 
