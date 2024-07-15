51035e019941fa3952fb4605b8df15310ccc5360
Haibib2 <66964771+Haibib2@users.noreply.github.com>
2024-07-15 00:20:51 -0400

    Add files via upload

---
+++ lang.js
@@ -0,0 +1,174 @@
+const translations = {
+    en: {
+        homepagetitle: `The Corpus of Conversational Uyghur`,
+        paragraph1: `Welcome! This collection of recordings represents a record of Modern Uyghur as used in casual conversation by diaspora speakers in the early 21 st century. The conversations are unscripted, unplanned, and recorded by the participants themselves in their homes or other spaces, with no researcher present.`,
+        paragraph2: `This corpus is intended to be a resource for anyone learning Uyghur on their own, teaching Uyghur to others, or doing research on various aspects of Uyghur language and linguistics. The contents may be used freely for any of these purposes. Most of the conversations were recorded with the participants’ phones. Some of them used the iPhone settings that record full-quality lossless audio; others used the regular settings that involve some compression of the sound. One conversation was recorded over Zoom. The metadata on the page for each conversation specifies the details.`,
+        paragraph3: `An explanation of the transcription conventions, the Uyghur Latin Script used in the transcripts, and the abbreviations used for the grammatical analysis can be found <span><a id="here-link" class="regularLink" href="misc/transcription_conventions.pdf">here</a></span>.`,
+        paragraph4: `Each transcript can also be downloaded as an .xml file and also as an .eaf file, which can be opened in the <span><a class="regularLink" href="https://archive.mpi.nl/tla/elan/download">program ELAN</a> along with the audio. To download a zip file containing all the recordings and transcripts, <span><a id="click-link" class="regularLink" href="zip/transcripts.zip">click here</a></span>.`,
+        contact: `<span>Contact: 
+    </span> 
+    <br id="email"> Email - <a class="footerLink" href="mfiddler@ucsb.edu">mfiddler@ucsb.edu</a> 
+    <br id="web"> Web design - Haibibullah Abdul-Kerim 
+    
+    <p id="footer-description">*Project organized by Michael Fiddler, in collaboration with the <a class="footerLink" href="https://uyghursfoundation.org/en/">Uyghur Projects Foundation</a></p> 
+    <hr class="visible-xs text-center"> `,
+        c1: `A Zoom call between two colleagues who both teach Uyghur language classes, discussing
+work, family, and a variety of other topics.`,
+        c2: `A husband and wife chat after a meal about their memories of music, life, and culture in their
+hometown.`,
+        c3: `Two friends take a walk in the park and chat about health, diet, and work.`,
+        c4: `After school, a young boy chats with his mom at home.`,
+        direction: 'ltr'
+    },
+    uy: {
+        homepagetitle: `«Uyghurche söhbet toplimi»`,
+        paragraph1: `Essalammu eleykum, tor bétimizge xush keldingiz! Hazirqi alimlargha qarighanda, kündilik söhbet insan tilining eng asasliq wezipisi bolup, kündilik söhbettiki gepni küzitish we tetqiq
+qilish herqandaq tilning alahidilikini chüshinish üchün bir achquch dur. Bu toplamdiki awazliq
+xatiriler 21-esirning deslepki yillirida di&#39;asporadiki uyghurlar kündilik söhbetlerde ishlitidighan
+hazirqi zaman uyghur tilining xatirilinishidur. Bu söhbetler aldin pilanlanmighan bolup,
+ishtirakchilar teripidin öyliride yaki bashqa jaylarda tetqiqat mes&#39;ulining nazariti bolmighan
+halette élip bérildi.`,
+        paragraph2: `Bu toplam uyghur tili ögengüchi hem ögetküchilerwe yaki uyghur tili boyiche tilshunasliq
+jehettiki tetqiqat qilidighan kishiler üchün bir menbe bolush meqsitide toplanghan bolup, mezkur
+toplamning mezmunlirini yuqirida déyilgen meqsetlerde erkin ishlitishke bolidu.`,
+        paragraph3: `Söhbetlerning köpinchisi qatnashquchilarning yanfonliri arqiliq xatirilengen bolup, beziliri yuqiri
+éniqliqta ünge élinghan bolsa yene beziliri adettiki awaz prislash funksiyesi bolghan tengshek
+halitide ünge élinghan. Yene bir söhbet zum supisida xatirilengen bolup her bir söhbetning
+sahibe melumatlirida, töwendikidek tepsilatlar yézilghan. Transkripsiye qilish qa&#39;idiliri,
+transkripsiyelerde ishlitilgen uyghur latin yéziqi we grammatikiliq tehlillerde ishlitilgen
+qisqartmilarning chüshendürülüshi bu yerdin <span><a id="here-link" class="regularLink" href="misc/transcription_conventions.pdf">tépilidu</a></span>.`,
+        paragraph4: `Her bir söhbetning transkripsiyisi hem <.xml> hem <eaf> format süpitide chüshürüwélishqa
+bolidu. Bu <.eaf> formattiki höjjetler munasiwetlik awazliq höjjiti bilen <span><a class="regularLink" href="https://archive.mpi.nl/tla/elan/download">&lt;ELAN&gt; namliq
+programmisi</a></span> arqiliq achqili bolidu. Barliq awazliq höjjetler we transkripsiyilirini <.zip> halitide
+bille chüshürüwélish üchün, bu <span><a id="click-link" class="regularLink" href="zip/transcripts.zip">yerni chéking</a></span>.`,
+        contact: `<span>Contact: 
+</span> 
+<br id="email"> Alaqe qilish üchün - <a class="footerLink" href="mfiddler@ucsb.edu">mfiddler@ucsb.edu</a> 
+<br id="web"> Torbet layihilesh - hebibullah abdukérim 
+
+<p id="footer-description">*Bu projékt kaliforniye uniwérsitét santa barbara (UCSB)-diki doktur aspiranti maykil fidlér
+(Michael Fiddler) bilen <a class="footerLink" href="https://uyghursfoundation.org/en/">uyghur projékt fondi teripidin</a> qilinghan.</p> 
+<hr class="visible-xs text-center"> `,
+        c1: `Zum arqiliq, uyghur tili ders ötidighan ikki xizmetdash xizmet, a&#39;ile we bashqa xil
+témilarda paranglishidu.`,
+        c2: `bir jüp er-ayal tamaq yégendin kéyin yurtining xelq naxshiliri, medeniyet, we bashqa
+xil ötken waqitlardiki xatiriliri heqqide paranglishidu.`,
+        c3: `ikki dost béghida aylinip, saghlamliq, yémek-ichmek we xizmet heqqide
+paranglishidu.`,
+        c4: `mekteptin qaytqandin kéyin, bir bala anisi bilen öyde paranglishidu.`,
+        direction: 'ltr'
+    },
+    ar: {
+        homepagetitle: `«ئۇيغۇرچە سۆھبەت توپلىمى»`,
+        paragraph1: `ئەسسالاممۇ ئەلەيكۇم، تور بېتىمىزگە خۇش كەلدىڭىز! ھازىرقى ئالىملارغا قارىغاندا، كۈندىلىك سۆھبەت ئىنسان تىلىنىڭ ئەڭ
+ئاساسلىق ۋەزىپىسى بولۇپ، كۈندىلىك سۆھبەتتىكى گەپنى كۈزىتىش ۋە تەتقىق قىلىش ھەرقانداق تىلنىڭ ئالاھىدىلىكىنى
+چۈشىنىش ئۈچۈن بىر ئاچقۇچ دۇر. بۇ توپلامدىكى ئاۋازلىق خاتىرىلەر 21-ئەسىرنىڭ دەسلەپكى يىللىرىدا دىئاسپورادىكى
+ئۇيغۇرلار كۈندىلىك سۆھبەتلەردە ئىشلىتىدىغان ھازىرقى زامان ئۇيغۇر تىلىنىڭ خاتىرىلىنىشىدۇر. بۇ سۆھبەتلەر ئالدىن
+پىلانلانمىغان بولۇپ، ئىشتىراكچىلار تەرىپىدىن ئۆيلىرىدە ياكى باشقا جايلاردا تەتقىقات مەسئۇلىنىڭ نازارىتى بولمىغان ھالەتتە
+ئېلىپ بېرىلدى.`,
+        paragraph2: `بۇ توپلام ئۇيغۇر تىلى ئۆگەنگۈچى ھەم ئۆگەتكۈچىلەرۋە ياكى ئۇيغۇر تىلى بويىچە تىلشۇناسلىق جەھەتتىكى تەتقىقات
+قىلىدىغان كىشىلەر ئۈچۈن بىر مەنبە بولۇش مەقسىتىدە توپلانغان بولۇپ، مەزكۇر توپلامنىڭ مەزمۇنلىرىنى يۇقىرىدا دېيىلگەن
+مەقسەتلەردە ئەركىن ئىشلىتىشكە بولىدۇ.`,
+        paragraph3: `سۆھبەتلەرنىڭ كۆپىنچىسى قاتناشقۇچىلارنىڭ يانفونلىرى ئارقىلىق خاتىرىلەنگەن بولۇپ، بەزىلىرى يۇقىرى ئېنىقلىقتا ئۈنگە
+ئېلىنغان بولسا يەنە بەزىلىرى ئادەتتىكى ئاۋاز پرىسلاش فۇنكسىيەسى بولغان تەڭشەك ھالىتىدە ئۈنگە ئېلىنغان. يەنە بىر
+سۆھبەت زۇم سۇپىسىدا خاتىرىلەنگەن بولۇپ ھەر بىر سۆھبەتنىڭ ساھىبە مەلۇماتلىرىدا، تۆۋەندىكىدەك تەپسىلاتلار يېزىلغان.
+ترانسكرىپسىيە قىلىش قائىدىلىرى، ترانسكرىپسىيەلەردە ئىشلىتىلگەن ئۇيغۇر لاتىن يېزىقى ۋە گرامماتىكىلىق تەھلىللەردە
+ئىشلىتىلگەن قىسقارتمىلارنىڭ چۈشەندۈرۈلۈشى بۇ يەردىن <span><a id="here-link" class="regularLink" href="misc/transcription_conventions.pdf">ئىشلىتىلگەن.</a></span>`,
+        paragraph4: `ھەر بىر سۆھبەتنىڭ ترانسكرىپسىيىسى ھەم &lt;.xml&gt; ھەم &lt;.eaf&gt; فورمات سۈپىتىدە چۈشۈرۈۋېلىشقا بولىدۇ. بۇ &lt;.eaf&gt;
+فورماتتىكى ھۆججەتلەر مۇناسىۋەتلىك ئاۋازلىق ھۆججىتى بىلەن <span><a class="regularLink" href="https://archive.mpi.nl/tla/elan/download">&lt;ELAN&gt;</a></span> ناملىق پروگراممىسى ئارقىلىق ئاچقىلى بولىدۇ.
+بارلىق ئاۋازلىق ھۆججەتلەر ۋە ترانسكرىپسىيىلىرىنى &lt;.zip&gt; ھالىتىدە بىللە چۈشۈرۈۋېلىش ئۈچۈن، بۇ يەرنى <span><a id="click-link" class="regularLink" href="zip/transcripts.zip">چېكىڭ.</a></span>`,
+        contact: `<span>Contact: 
+</span> 
+<br id="email">ئالاقە قىلىش ئۈچۈن- <a class="footerLink" href="mfiddler@ucsb.edu">mfiddler@ucsb.edu</a> 
+<br id="web"> توربەت لايىھىلەش: ھەبىبۇللاھ ئابدۇكېرىم
+
+<p id="footer-description">بۇ پروجېكت كالىفورنىيە ئۇنىۋېرسىتېت سانتا باربارا (UCSB)-دىكى دوكتۇر ئاسپىرانتى مايكىل فىدلېر (Michael Fiddler) بىلەنن.<a class="footerLink" href="https://uyghursfoundation.org/en/">ئۇيغۇر پروجېكت فوندى</a> تەرىپىدىن قىلىنغا.</p> 
+<hr class="visible-xs text-center"> `,
+        c1: `زۇم ئارقىلىق، ئۇيغۇر تىلى دەرس ئۆتىدىغان ئىككى خىزمەتداش خىزمەت، ئائىلە ۋە باشقا خىل تېمىلاردا
+پاراڭلىشىدۇ`,
+        c2: `بىر جۈپ ئەر-ئايال تاماق يېگەندىن كېيىن يۇرتىنىڭ خەلق ناخشىلىرى، مەدەنىيەت، ۋە باشقا خىل ئۆتكەن
+ۋاقىتلاردىكى خاتىرىلىرى ھەققىدە پاراڭلىشىدۇ`,
+        c3: `ئىككى دوست بېغىدا ئايلىنىپ، ساغلاملىق، يېمەك-ئىچمەك ۋە خىزمەت ھەققىدە پاراڭلىشىدۇ`,
+        c4: `مەكتەپتىن قايتقاندىن كېيىن، بىر بالا ئانىسى بىلەن ئۆيدە پاراڭلىشىدۇ`,
+        direction: 'rtl'
+    },
+    cy: {
+        homepagetitle: `&quot;уйғурчә сөһбәт топлими&quot;`,
+        paragraph1: `әссаламму әләйкум, тор бетимизгә хуш кәлдиңиз! һазирқи алимларға қариғанда, күндилик
+сөһбәт инсан тилиниң әң асаслиқ вәзиписи болуп, күндилик сөһбәттики гәпни күзитиш вә
+тәтқиқ қилиш һәрқандақ тилниң алаһидиликини чүшиниш үчүн бир ачқуч дур. бу
+топламдики авазлиқ хатириләр 21-әсирниң дәсләпки йиллирида диаспорадики уйғурлар
+күндилик сөһбәтләрдә ишлитидиған һазирқи заман уйғур тилиниң хатирилинишидур. бу
+сөһбәтләр алдин пиланланмиған болуп, иштиракчилар тәрипидин өйлиридә яки башқа
+җайларда тәтқиқат мәсулиниң назарити болмиған һаләттә елип берилди.`,
+        paragraph2: `бу топлам уйғур тили өгәнгүчи һәм өгәткүчиләрвә яки уйғур тили бойичә тилшунаслиқ
+җәһәттики тәтқиқат қилидиған кишиләр үчүн бир мәнбә болуш мәқситидә топланған
+болуп, мәзкур топламниң мәзмунлирини юқирида дейилгән мәқсәтләрдә әркин
+ишлитишкә болиду.`,
+        paragraph3: `сөһбәтләрниң көпинчиси қатнашқучиларниң янфонлири арқилиқ хатириләнгән болуп,
+бәзилири юқири ениқлиқта үнгә елинған болса йәнә бәзилири адәттики аваз прислаш
+функсийәси болған тәңшәк һалитидә үнгә елинған. йәнә бир сөһбәт зум суписида
+хатириләнгән болуп һәр бир сөһбәтниң саһибә мәлуматлирида, төвәндикидәк тәпсилатлар
+йезилған. транскрипсийә қилиш қаидилири, транскрипсийәләрдә ишлитилгән уйғур латин
+йезиқи вә грамматикилиқ тәһлилләрдә ишлитилгән қисқартмиларниң чүшәндүрүлүши бу
+йәрдин <span><a id="here-link" class="regularLink" href="misc/transcription_conventions.pdf">тепилиду.</a></span>.`,
+        paragraph4: `һәр бир сөһбәтниң транскрипсийиси һәм &lt;.xml&gt; һәм &lt;.eaf&gt; формат сүпитидә
+чүшүрүвелишқа болиду. бу &lt;.eaf&gt; форматтики һөҗҗәтләр мунасивәтлик авазлиқ
+<span><a class="regularLink" href="https://archive.mpi.nl/tla/elan/download">һөҗҗити
+билән &lt;ELAN&gt;</a> намлиқ программиси арқилиқ ачқили болиду. барлиқ авазлиқ һөҗҗәтләр
+вә транскрипсийилирини &lt;.zip&gt; һалитидә биллә чүшүрүвелиш үчүн, бу
+<span><a id="click-link" class="regularLink" href="zip/transcripts.zip">йәрни чекиң.</a></span>.`,
+        contact: `<span>Contact: 
+</span> 
+<br id="email">алақә қилиш үчүн - <a class="footerLink" href="mfiddler@ucsb.edu">mfiddler@ucsb.edu</a> 
+<br id="web">торбәт лайиһиләш: һәбибуллаһ абдукерим
+
+<p id="footer-description">*бу проҗект калифорнийә университет санта барбара (UCSB)-дики доктур аспиранти
+майкил фидлер (Michael Fiddler) билән <a class="footerLink" href="https://uyghursfoundation.org/en/">уйғур проҗект фонди</a> тәрипидин қилинған.</p> 
+<hr class="visible-xs text-center"> `,
+        c1: `зум арқилиқ, уйғур тили дәрс өтидиған икки хизмәтдаш хизмәт, аилә вә башқа
+хил темиларда параңлишиду.`,
+        c2: `бир җүп әр-аял тамақ йегәндин кейин юртиниң хәлқ нахшилири, мәдәнийәт, вә
+башқа хил өткән вақитлардики хатирилири һәққидә параңлишиду.`,
+        c3: `икки дост беғида айлинип, сағламлиқ, йемәк-ичмәк вә хизмәт һәққидә
+параңлишиду.`,
+        c4: `мәктәптин қайтқандин кейин, бир бала аниси билән өйдә параңлишиду.`,
+        direction: 'ltr'
+    }
+};
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const savedLanguage = localStorage.getItem('selectedLanguage') || 'en';
+    const buttons = document.querySelectorAll('.lang-option');
+    buttons.forEach(button => {
+        if (button.textContent.trim() === translations[savedLanguage].welcomeText.split(' ')[0]) {
+            button.classList.add('selected');
+        }
+    });
+    switchLanguage(savedLanguage, document.querySelector('.lang-option.selected'));
+});
+
+function switchLanguage(language, element) {
+    const translation = translations[language];
+    document.getElementById('homepage-title').innerHTML = translations[language].homepagetitle;
+    document.getElementById('paragraph-1').innerHTML = translations[language].paragraph1;
+    document.getElementById('paragraph-2').innerHTML = translations[language].paragraph2;
+    document.getElementById('paragraph-3').innerHTML = translations[language].paragraph3;
+    document.getElementById('paragraph-4').innerHTML = translations[language].paragraph4;
+    document.getElementById('contact').innerHTML = translations[language].contact;
+    document.getElementById('c1-des').innerHTML = translations[language].c1;
+    document.getElementById('c2-des').innerHTML = translations[language].c2;
+    document.getElementById('c3-des').innerHTML = translations[language].c3;
+    document.getElementById('c4-des').innerHTML = translations[language].c4;
+
+    let textContainer = document.getElementById('main-text');
+    textContainer.className = `text-container ${translation.direction === 'rtl' ? 'rtl' : ''}`;
+    textContainer = document.getElementById('footer-text');
+    textContainer.className = `text-container ${translation.direction === 'rtl' ? 'rtl' : ''}`;
+
+    const buttons = document.querySelectorAll('.lang-option');
+    buttons.forEach(button => {
+        button.classList.remove('selected');
+    });
+    element.classList.add('selected');
+}
